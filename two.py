32f662b5311fd7761ca9571a2ae69dddc36bd7cf
liqima <31846112+liqima@users.noreply.github.com>
2018-02-11 17:08:00 +0800

    Delete two.py

--- two.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        my_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in my_dict:
-                return [my_dict[nums[i]], i]
-            else:
-                my_dict[target - nums[i]] = i
